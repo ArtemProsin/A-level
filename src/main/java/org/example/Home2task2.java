4801e70b8c5cbe454ef1cdfae69c9eb6f299b9a5
ArtemProsin <a.prosin16@gmail.com>
2024-01-04 12:32:22 +0200

    add initial implementation

--- src/main/java/org/example/Home2task2.java
+++ src/main/java/org/example/Home2task2.java
@@ -7,7 +7,7 @@ public class Home2task2 {
 
         double area = calculateTriangleArea(pointA, pointB, pointC);
 
-        System.out.println("Площадь треугольника: " + area);
+        System.out.println("Triangle Area: " + area);
     }
     public static double calculateTriangleArea(double[] A, double[] B, double[] C) {
         double area = 0.5 * Math.abs(A[0] * (B[1] - C[1]) + B[0] * (C[1] - A[1]) + C[0] * (A[1] - B[1]));
