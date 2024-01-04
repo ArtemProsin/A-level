23a93a4d81f1066b08d2e3f97897a58de89afa6b
ArtemProsin <a.prosin16@gmail.com>
2024-01-04 12:38:46 +0200

    add initial implementation

---
+++ src/main/java/org/example/task3.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class task3 {
+    public static void main(String[] args) {
+        int number = 4567;
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+    }
+}
+
+
