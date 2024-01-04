998778113c136ccebd44ea20db7af7fdfa231098
ArtemProsin <a.prosin16@gmail.com>
2024-01-04 12:41:36 +0200

    add initial implementation

---
+++ src/main/java/org/example/task4.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class task4 {
+    public static void main(String[] args) {
+        double num1 = -5;
+        double num2 = 7;
+        double num3 = 11;
+        double minAbsNumber = Math.min(Math.abs(num1), Math.min(Math.abs(num2), Math.abs(num3)));
+
+        if (Math.abs(num1) == minAbsNumber) {
+            System.out.println("Number with the smallest absolute value: " + num1);
+        } else if (Math.abs(num2) == minAbsNumber) {
+            System.out.println("Number with the smallest absolute value: " + num2);
+        } else {
+            System.out.println("Number with the smallest absolute value: " + num3);
+        }
+    }
+}
+
