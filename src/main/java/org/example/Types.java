46b0e9f67c60308b8cb2ccb0b1a947dbbc6bcf94
ArtemProsin <a.prosin16@gmail.com>
2024-01-04 12:07:29 +0200

    add initial implementation

--- src/main/java/org/example/Types.java
+++ src/main/java/org/example/Types.java
@@ -1,9 +1,1 @@
-public class Types {
- public static void main(String[] args) {
-  int decimal = 77;
-  String binary = Integer.toBinaryString(decimal);
-          System.out.println(decimal);
-          System.out.println(binary);
- }
-}
 
