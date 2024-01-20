ce7908fcc869e51bd5f2213f9de1c8362232a8a6
ArtemProsin <a.prosin16@gmail.com>
2024-01-20 17:33:15 +0200

    add initial implementation

---
+++ src/main/java/ua/Prosin/HW3/task2.java
@@ -0,0 +1,18 @@
+package ua.Prosin.HW3;
+
+public class task2 {
+    public static void main(String[] args) {
+        String originalString = "A man a plan a canal Panama";
+        String stringWithoutSpace = originalString.replaceAll("\\s", "");
+
+        if (isPalindrome(stringWithoutSpace)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+    public static boolean isPalindrome(String str){
+        String reversedString = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reversedString);
+    }
+}
