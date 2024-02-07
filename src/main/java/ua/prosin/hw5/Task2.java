a68dc185387ce1dcac51b61f721bd50e607fec0a
ArtemProsin <a.prosin16@gmail.com>
2024-02-07 19:01:59 +0200

    add initial implementation

--- src/main/java/ua/prosin/hw5/Task2.java
+++ src/main/java/ua/prosin/hw5/Task2.java
@@ -3,7 +3,7 @@ package ua.prosin.hw5;
 public class Task2 {
 
     public static void main(String[] args) {
-        int[][] array = {{66, 4, 1}, {10, 2, 55}, {15, 11, 9}};
+        int[] array = {66, 4, 1, 10, 2, 55, 15, 11, 9};
 
         System.out.println("Original array:");
         printArray(array);
@@ -13,14 +13,12 @@ public class Task2 {
         System.out.println("Array is ordered in descending order: " + isOrdered);
     }
 
-    private static boolean checkArrayDescendingOrder(int[][] array) {
-        int[][] copyArray = copyArray(array);
+    private static boolean checkArrayDescendingOrder(int[] array) {
+        int[] copyArray = copyArray(array);
 
-        for (int i = 0; i < copyArray.length; i++) {
-            for (int j = 1; j < copyArray[i].length; j++) {
-                if (copyArray[i][j - 1] < copyArray[i][j]) {
-                    return false;
-                }
+        for (int i = 1; i < copyArray.length; i++) {
+            if (copyArray[i - 1] < copyArray[i]) {
+                return false;
             }
         }
 
@@ -30,26 +28,17 @@ public class Task2 {
         return true;
     }
 
-    private static int[][] copyArray(int[][] array) {
-        int numRows = array.length;
-        int numCols = array[0].length;
-        int[][] copy = new int[numRows][numCols];
+    private static int[] copyArray(int[] array) {
+        int[] copy = new int[array.length];
 
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                copy[i][j] = array[i][j];
-            }
-        }
+        System.arraycopy(array, 0, copy, 0, array.length);
 
         return copy;
     }
 
-    private static void printArray(int[][] array) {
-        for (int[] row : array) {
-            for (int value : row) {
-                System.out.print(value + " ");
-            }
-            System.out.println();
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
