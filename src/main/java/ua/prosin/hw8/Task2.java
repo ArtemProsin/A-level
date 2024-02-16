15273c158a94534131865b2c3f4bc14b8d3a145a
ArtemProsin <a.prosin16@gmail.com>
2024-02-16 11:09:14 +0200

    edits made

--- src/main/java/ua/prosin/hw8/Task2.java
+++ src/main/java/ua/prosin/hw8/Task2.java
@@ -1,18 +1,20 @@
 package ua.prosin.hw8;
+
 @FunctionalInterface
 interface Pow {
     int pow(int number, int pow);
 }
+
 public class Task2 {
     public static void main(String[] args) {
         Pow power = (number, pow) -> {
             int result = number;
-          for(int i = 1; i < pow; i++){
-              result *= number;
-          }
-          return result;
+            for (int i = 1; i < pow; i++) {
+                result *= number;
+            }
+            return result;
         };
-       int result = power.pow(4,2);
+        int result = power.pow(4, 2);
         System.out.println(result);
     }
 }
