6d1bd3b5461911613ad2d7f76bf0f46c31f09650
ArtemProsin <a.prosin16@gmail.com>
2024-02-16 11:06:46 +0200

    add initial implementation

--- src/main/java/ua/prosin/hw8/Task1.java
+++ src/main/java/ua/prosin/hw8/Task1.java
@@ -1,11 +1,8 @@
 package ua.prosin.hw8;
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
-        Student[] students = {
-                new Student("Alex", "Shark", "Group D", 3.9),
-                new Aspirant("Roma", "Nova", "Group F", 5, "3")
-        };
+        Student[] students = {new Student("Alex", "Shark", "Group D", 3.9), new Aspirant("Roma", "Nova", "Group F", 5, "3")};
 
         for (Student student : students) {
             System.out.println(student.firstName + " " + student.lastName + ": " + student.getScholarship() + " грн");
