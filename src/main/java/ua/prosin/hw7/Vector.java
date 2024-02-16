17e430006120d46e9e80354c49bb55a0b8f1b4a9
ArtemProsin <a.prosin16@gmail.com>
2024-02-16 11:33:54 +0200

    edits made

--- src/main/java/ua/prosin/hw7/Vector.java
+++ src/main/java/ua/prosin/hw7/Vector.java
@@ -2,20 +2,20 @@ package ua.prosin.hw7;
 
 import java.util.Random;
 
-public class Task2 {
+public class Vector {
     private final double x;
     private final double y;
     private final double z;
 
-    public Task2(double x, double y, double z) {
+    public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
     public static void main(String[] args) {
-        Task2 vector1 = new Task2(1, 2, 3);
-        Task2 vector2 = new Task2(4, 5, 6);
+        Vector vector1 = new Vector(1, 2, 3);
+        Vector vector2 = new Vector(4, 5, 6);
 
         // Test methods
         System.out.println("Length of vector1: " + vector1.calculateLength());
@@ -28,9 +28,9 @@ public class Task2 {
         System.out.println("Difference of vector1 and vector2: " + vector1.calculateDifference(vector2));
 
         int n = 5;
-        Task2[] randomVectors = Task2.generateRandomVectors(n);
+        Vector[] randomVectors = Vector.generateRandomVectors(n);
         System.out.println("Random vectors:");
-        for (Task2 vector : randomVectors) {
+        for (Vector vector : randomVectors) {
             System.out.println(vector);
         }
     }
@@ -39,41 +39,41 @@ public class Task2 {
         return Math.sqrt(x * x + y * y + z * z);
     }
 
-    public Task2 calculateCrossProduct(Task2 other) {
+    public Vector calculateCrossProduct(Vector other) {
         double newX = y * other.z - z * other.y;
         double newY = z * other.x - x * other.z;
         double newZ = x * other.y - y * other.x;
-        return new Task2(newX, newY, newZ);
+        return new Vector(newX, newY, newZ);
     }
 
-    public double calculateCosineAngle(Task2 other) {
+    public double calculateCosineAngle(Vector other) {
         double dotProduct = x * other.x + y * other.y + z * other.z;
         double lengthProduct = calculateLength() * other.calculateLength();
         return dotProduct / lengthProduct;
     }
 
-    public Task2 calculateSum(Task2 other) {
+    public Vector calculateSum(Vector other) {
         double newX = x + other.x;
         double newY = y + other.y;
         double newZ = z + other.z;
-        return new Task2(newX, newY, newZ);
+        return new Vector(newX, newY, newZ);
     }
 
-    public Task2 calculateDifference(Task2 other) {
+    public Vector calculateDifference(Vector other) {
         double newX = x - other.x;
         double newY = y - other.y;
         double newZ = z - other.z;
-        return new Task2(newX, newY, newZ);
+        return new Vector(newX, newY, newZ);
     }
 
-    public static Task2[] generateRandomVectors(int N) {
+    public static Vector[] generateRandomVectors(int N) {
         Random random = new Random();
-        Task2[] vectors = new Task2[N];
+        Vector[] vectors = new Vector[N];
         for (int i = 0; i < N; i++) {
             double x = random.nextDouble();
             double y = random.nextDouble();
             double z = random.nextDouble();
-            vectors[i] = new Task2(x, y, z);
+            vectors[i] = new Vector(x, y, z);
         }
         return vectors;
     }
